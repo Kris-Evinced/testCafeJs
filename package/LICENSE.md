3f6f52992a7c990f6b4065e2198a0e7e7c3dedb4
kris <kristen.sutherland@evinced.com>
2025-03-06 16:12:34 -0800

    commit take 4

---
+++ package/LICENSE.md
@@ -0,0 +1,2 @@
+This software is licensed under our EULA which is available at: https://www.evinced.com/eula
+OSS disclosure can be found here: https://evinced.com/oss-disclosure
---
+++ tests/evinced-test1.test.js
@@ -0,0 +1,30 @@
+const { fixture } = require("testcafe");
+const { ClientFunction } = require("testcafe");
+const { setCredentials, EvincedSDK } = require("@evinced/js-testcafe-sdk");
+
+
+let evinced;
+fixture`Evinced Demo site tests`.page`https://demo.evinced.com/`
+    .before(async (ctx) => {
+        // Set online credentials
+        setCredentials({
+            serviceId: process.env.EVINCED_SERVICE_ID,
+            secret: process.env.EVINCED_API_KEY,
+        })
+    })
+    .beforeEach(async (t) => {
+        // Initialize Evinced SDK
+        evinced = new EvincedSDK(t);
+        // Start the Evinced engine
+        await evinced.evStart();
+    })
+    .afterEach(async (t) => {
+        const issues = await evinced.evStop();
+    });
+
+test("Demo page. Single page analyze", async (t) => {
+    // Example action to verify we are on the correct page
+    await t
+        .expect(ClientFunction(() => document.title)())
+        .eql("Home | Evinced, Demo site");
+});
