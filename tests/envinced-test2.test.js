2c45aa55d64573dbddf7d6aae2c5e8c5b185db66
kris <kristen.sutherland@evinced.com>
2025-03-18 14:27:11 -0700

    cleaning up the testCafeJs repo - removing node_modules

---
+++ tests/envinced-test2.test.js
@@ -0,0 +1,32 @@
+const { fixture } = require("testcafe");
+const { ClientFunction } = require("testcafe");
+const { setCredentials, EvincedSDK } = require("@evinced/js-testcafe-sdk");
+
+let evinced;
+fixture`Evinced Demo site tests`.page`https://demo.evinced.com/`
+    .before(async (ctx) => {
+        // Set online credentials
+        setCredentials({
+            serviceId: process.env.EVINCED_SERVICE_ID,
+            secret: process.env.EVINCED_API_KEY,
+        })
+    })
+    .beforeEach(async (t) => {
+        // Initialize Evinced SDK
+        evinced = new EvincedSDK(t);
+        // Start the Evinced engine
+        await evinced.evStart();
+    })
+    .afterEach(async (t) => {
+        const issues = await evinced.evAnalyze()
+        console.log("evSafeFile")
+        await evinced.evSaveFile(issues, 'html', './evReports/evincedTest.html');
+
+    });
+
+test('Demo page. evAnalyze', async t => {
+    const evinced = new EvincedSDK(t);
+
+    const issues = await evinced.evAnalyze();
+    await t.expect(issues.length).eql(6);
+});
